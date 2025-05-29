801917ab54522275ad788cf9a7b9b72c99ffafd3
Kirthi-2000 <135959708+Kirthi-2000@users.noreply.github.com>
2025-05-29 15:28:19 +0530

    IS_Recovery_IS_Completed_Table

--- Brass_Qc/views.py
+++ Brass_Qc/views.py
@@ -9,9 +9,17 @@ class BrassPickTableView(APIView):
     def get(self, request):
         return render(request, 'Brass_Qc/Brass_PickTable.html')
     
+class BrassCompletedView(APIView):
+    def get(self, request):
+        return render(request, 'Brass_Qc/Brass_Completed.html')
+    
 class IqfPickTableView(APIView):
     def get(self, request):
         return render(request, 'IQF/Iqf_PickTable.html')
+
+class IqfCompletedTableView(APIView):
+    def get(self, request):
+        return render(request, 'IQF/Iqf_Completed.html')
     
 class IqpRejectTableView(APIView):
     def get(self, request):
@@ -22,9 +30,17 @@ class R_BrassPickTableView(APIView):
     def get(self, request):
         return render(request, 'Recovery_BQ/Recovery_BQ_PickTable.html')
     
+class R_BrassCompletedView(APIView):
+    def get(self, request):
+        return render(request, 'Recovery_BQ/Recovery_BQ_Completed.html')
+    
 class R_IqfPickTableView(APIView):
     def get(self, request):
         return render(request, 'Recovery_IQF/Recovery_Iqf_PickTable.html')
+    
+class R_IqfCompletedTableView(APIView):
+    def get(self, request):
+        return render(request, 'Recovery_IQF/Recovery_Iqf_Completed.html')
     
 class R_IqpRejectTableView(APIView):
     def get(self, request):
